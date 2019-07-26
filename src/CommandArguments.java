5c0d6ceaa349f7084c460ec17b6d292d1c56dbb3
vkku <kumar.vivekten@gmail.com>
2019-07-26 23:06:10 +0530

    Add cmd params

--- src/CommandArguments.java
+++ src/CommandArguments.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CommandArguments {
   static Integer sum = 0;
@@ -11,10 +10,13 @@ public class CommandArguments {
     while (size > 0) {
       paramList.add(args[--size]);
     }
-    List<Integer> intParam = paramList.stream().map(Integer::parseInt).collect(Collectors.toList());
-    intParam.forEach(val -> {
-      sum += val;
+    paramList.stream().map(Integer::parseInt).flatMap(s -> {
+
+      sum += s;
+      return null;
+
     });
-    System.out.println(sum);
+
+    System.out.println("Sum = " + sum);
   }
 }
